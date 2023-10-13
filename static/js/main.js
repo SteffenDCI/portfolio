ba60f1da6cc69cde1cb34498227f2f580d523954
Steffen P <steffen.pretzel@gmx.de>
2023-10-13 15:22:51 +0200

    Initial commit

---
+++ static/js/main.js
@@ -0,0 +1,11 @@
+function changeTheme(theme) {
+    if (theme == 'dark')
+    {
+        document.body.style.backgroundColor = 'black';
+        document.body.style.color = 'white';
+
+    } else {
+        document.body.style.backgroundColor = '';
+        document.body.style.color = '';
+    }
+}
